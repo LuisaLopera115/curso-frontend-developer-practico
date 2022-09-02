d913365441ec5300039a8b8568869fcf08eea496
Luisa Fernanda Lopera Gallo <Luisalopera.developer@gmail.com>
2022-09-02 09:59:26 -0500

    Car shopping in index

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ const destokeMenu = document.querySelector('.desktop-menu');
 const burguerMenu= document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
+const shoppingCarIconMobile= document.querySelector('.navbar-shopping-cart');
+const itemsInCarMobile=document.querySelector('.product-detail');
+
 menuEmail.addEventListener('click',ToogleDeskmanu);
 burguerMenu.addEventListener('click',ToogleMobilekmanu);
+shoppingCarIconMobile.addEventListener('click',ToggleItemsInCarMobile);
 
 function ToogleDeskmanu(){
     console.log('desktopmenu cliked');
@@ -13,6 +17,23 @@ function ToogleDeskmanu(){
 }
 
 function ToogleMobilekmanu(){
+
+    if (!itemsInCarMobile.classList.contains('inactive')) {
+        itemsInCarMobile.classList.add('inactive')
+    }
     console.log('Mobiletopmenu cliked');
     mobileMenu.classList.toggle('inactive');
-}   
+}  
+
+function ToggleItemsInCarMobile(){
+
+    if (!mobileMenu.classList.contains('inactive')) {
+        mobileMenu.classList.add('inactive')
+    }
+    console.log('ToggleItemsInCarMobile cliked');
+    itemsInCarMobile.classList.toggle('inactive');
+} 
+
+
+
+
